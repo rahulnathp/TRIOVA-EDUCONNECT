72202dbef52f24584d81f8eb2ced5b3642f7c1f8
Rahul Nath <rahulnathpremnath@gmail.com>
2026-04-03 16:05:00 +0530

    Cors functionality added

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   // Debug environment variables
-  console.log(' Environment Variables:');
-  console.log('DB_HOST:', process.env.DB_HOST);
-  console.log('DB_PORT:', process.env.DB_PORT);
-  console.log('DB_USERNAME:', process.env.DB_USERNAME);
-  console.log('DB_DATABASE:', process.env.DB_DATABASE);
-  console.log('NODE_ENV:', process.env.NODE_ENV);
   
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'],
+    origin: '*', // allow all (for testing)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
