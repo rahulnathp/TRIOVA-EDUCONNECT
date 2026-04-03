40588192a1247adb900a32127b8e56301fcf3bbf
Rahul Nath <rahulnathpremnath@gmail.com>
2026-04-03 12:54:47 +0530

    Added new changes in app module

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'triova',
+  username: process.env.DB_USERNAME ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_DATABASE ,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production',
   ssl: {
